051dcf4e13397f2d6619d6218ff54a6baff5b6fe
Krzysztof Dziezyk <krzysztof.dziezyk@gmail.com>
2017-01-19 13:40:37 +0100

    angular.module function

--- src/loader.js
+++ src/loader.js
@@ -2,8 +2,46 @@
 'use strict';
 
 function setupModuleLoader(window) {
+
 	function ensure(obj, name, factory) {
 		return (obj[name] || (obj[name] = factory()) );
 	}
+
 	var angular = ensure(window, "angular", Object);
+
+
+	//factory for the function angular.module; called only once
+	ensure(angular, 'module', function() { //factory
+		var modules = {};
+		return function(name, requires) { //module function
+			if(requires) {	
+				return createModule(name, requires, modules);
+			} else {
+				return gotModule(name, modules);
+			}
+		}
+	});
+
+	function createModule(name, requires, modules) {
+		if(name === 'hasOwnProperty') {
+			throw 'module name : \"hasOwnProperty\" is not permited.';
+		}
+		var moduleInstance = {
+			name: name,
+			requires: requires,
+		};
+
+		modules[name] = moduleInstance;
+
+		return moduleInstance;
+	}
+
+	function gotModule(name, modules) {
+		if(modules.hasOwnProperty(name)) {
+			return modules[name];
+		} else {
+			throw 'Module \"' + name + '\" does not exist.';
+		}
+		
+	}
 }
--- test/loader_spec.js
+++ test/loader_spec.js
@@ -20,4 +20,62 @@ describe('setupModuleLoader', function() {
 		expect(window.angular).toBe(ng);
 	});
 
+	it('exposes the angular module function', function() {
+		setupModuleLoader(window);
+		expect(window.angular.module).toBeDefined();
+	});
+
+	it('creates angular module function just once', function() {
+		setupModuleLoader(window);
+		var module = window.angular.module;
+		setupModuleLoader(window);
+		expect(window.angular.module).toBe(module);
+	});
+
+});
+
+describe('modules', function() {
+
+	beforeEach(function() {
+		setupModuleLoader(window);
+	});
+
+	it('allows registering a module', function() {
+		var module = angular.module('mod1', []);
+		expect(module).toBeDefined();
+		expect(module.name).toBe('mod1');
+	});
+
+	it('replaces a module when registered the same again', function() {
+		var module1 = angular.module('mod1', []);
+		var module2 = angular.module('mod1', []);
+		expect(module1).not.toBe(module2);
+	});
+
+	it('attaches the requires array to the registered module', function() {
+		var module = angular.module('mod1', ['mod2']);
+		expect(module).toBeDefined();
+		expect(module.requires).toEqual(['mod2']);
+	});
+
+	it('allows getting a module', function() {
+		var module1 = angular.module('mod1', []);
+		var module2 = angular.module('mod1');
+		expect(module2).toBeDefined();
+		expect(module1).toBe(module2);
+	});
+
+	it('allows getting a module', function() {
+		expect(function() {
+			angular.module('moduleXYZ');
+		}).toThrow();
+	});
+
+	it('does not allow a module to be called hasOwnProperty', function() {
+		expect(function() {
+			angular.module('hasOwnProperty', []);
+		}).toThrow();
+	});
+
+
 });
